42e90e134ac584c169015b2b077edd4b552393aa
Antonella Ravaioli <5211016-antorava1@users.noreply.gitlab.com>
2021-09-16 22:07:48 -0300

    Hero

---
+++ paisaflix/src/components/Hero.js
@@ -0,0 +1,20 @@
+import '../styles/hero.css';
+import React from 'react';
+
+function Hero () {
+    return (
+        <div>
+            <div>
+                <h1 className="movie-title">Blade Runner</h1>
+                <h6 className="stars">Estrellas</h6>
+                <h6 className="movie-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</h6>
+                <h6 className="movie-genre">Genre: Action</h6>
+                <h6 className="movie-duration">Duration: 2 hr 45 mins</h6>
+                <h6 className="movie-rating">Rating: 4.5</h6>
+            </div>
+            <button className="btn">Watch Now</button>
+        </div>
+    )
+}
+
+export default Hero;
