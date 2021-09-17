7123d96196cf1a1ca8d70b954682f06edeeb6605
Antonella Ravaioli <5211016-antorava1@users.noreply.gitlab.com>
2021-09-17 03:14:08 -0300

    Footer

---
+++ paisaflix/src/components/Footer.js
@@ -0,0 +1,43 @@
+import '../styles/footer.css';
+import React from 'react';
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
+
+function Footer () {
+    return(
+        <footer>
+            <section className="section1">
+                <h4 className="title">Paisaflix</h4>
+                <h6 className="subtitle">Join Newsletters</h6>
+                <InputGroup className="input">
+                    <FormControl className="form-text" placeholder="Insert your mail here" aria-label="insert your mail here"/>
+                    <Button className="btn-input"></Button>
+                </InputGroup>
+            </section>
+            <section className="section2">
+                <h4 className="title2">Product</h4>
+                <h5 className="sub2">Movies</h5>
+                <h5 className="sub3">TV Show</h5>
+                <h5 className="sub4">Videos</h5>
+            </section>
+            <section className="section3">
+                <h4 className="title3">Media Group</h4>
+                <h5 className="sub5">Nice Studio</h5>
+                <h5 className="sub6">Nice News</h5>
+                <h5 className="sub7">Nice TV</h5>
+            </section>
+            <section className="section4">
+                <h4 className="title4">Sitemap</h4>
+                <h5 className="sub8">About</h5>
+                <h5 className="sub9">Careers</h5>
+                <h5 className="sub10">Press</h5>
+            </section>
+            <section className="section5">
+                <h5 className="sub11">8819 Enrique Martinez, Bs As., 90280</h5>
+                <h5 className="sub12">hola@paisanos.io</h5>
+                <h5 className="sub13">+271 386-647-3637</h5>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer;
