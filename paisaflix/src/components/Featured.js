0703e4021b1791dc846d67586543919ba68f6f98
Antonella Ravaioli <5211016-antorava1@users.noreply.gitlab.com>
2021-09-20 02:10:53 -0300

    íconos

--- paisaflix/src/components/Featured.js
+++ paisaflix/src/components/Featured.js
@@ -1,6 +1,8 @@
 import '../styles/featured.css';
 import React from 'react';
 import { Card, Badge } from 'react-bootstrap';
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faClock, faEye } from '@fortawesome/free-solid-svg-icons';
 
 function Featured () {
     return (
@@ -9,32 +11,67 @@ function Featured () {
             <h4 className="view-more">View More</h4>
             <Card className="first-movie">
                 <Badge className="badge1">Género</Badge>
-                <Card.Text className="duration1">1hr 24mins</Card.Text>
-                <Card.Text className="views1">21.3K views</Card.Text>
+                <Card.Text className="duration1">
+                    <FontAwesomeIcon classname="clock1" icon={faClock}/>&nbsp;
+                    1hr 24mins
+                </Card.Text>
+                
+                <Card.Text className="views1">
+                    <FontAwesomeIcon className="eye1" icon={faEye}/>&nbsp;
+                    21.3K views
+                </Card.Text>
                 <Card.Title className="movie-title1">Star Wars - The Last Jedi</Card.Title>
             </Card>
             <Card className="second-movie">
                 <Badge className="badge2">Género</Badge>
-                <Card.Text className="duration2">1hr 24mins</Card.Text>
-                <Card.Text className="views2">21.3K views</Card.Text>
+                <Card.Text className="duration2">
+                    <FontAwesomeIcon classname="clock1" icon={faClock}/>&nbsp;
+                    1hr 24mins
+                </Card.Text>
+                
+                <Card.Text className="views1">
+                    <FontAwesomeIcon className="eye1" icon={faEye}/>&nbsp;
+                    21.3K views
+                </Card.Text>
                 <Card.Title className="movie-title2">Triggered</Card.Title>
             </Card>
             <Card className="third-movie">
                 <Badge className="badge3">Género</Badge>
-                <Card.Text className="duration3">1hr 24mins</Card.Text>
-                <Card.Text className="views3">21.3K views</Card.Text>
+                <Card.Text className="duration3">
+                    <FontAwesomeIcon classname="clock1" icon={faClock}/>&nbsp;
+                    1hr 24mins
+                </Card.Text>
+                
+                <Card.Text className="views1">
+                    <FontAwesomeIcon className="eye1" icon={faEye}/>&nbsp;
+                    21.3K views
+                </Card.Text>
                 <Card.Title className="movie-title3">Matrix</Card.Title>
             </Card>
             <Card className="fourth-movie">
                 <Badge className="badge4">Género</Badge>
-                <Card.Text className="duration4">1hr 24mins</Card.Text>
-                <Card.Text className="views4">21.3K views</Card.Text>
+                <Card.Text className="duration4">
+                    <FontAwesomeIcon classname="clock1" icon={faClock}/>&nbsp;
+                    1hr 24mins
+                </Card.Text>
+                
+                <Card.Text className="views1">
+                    <FontAwesomeIcon className="eye1" icon={faEye}/>&nbsp;
+                    21.3K views
+                </Card.Text>
                 <Card.Title className="movie-title4">Dunkirk</Card.Title>
             </Card>
             <Card className="fifth-movie">
                 <Badge className="badge5">Género</Badge>
-                <Card.Text className="duration5">1hr 24mins</Card.Text>
-                <Card.Text className="views5">21.3K views</Card.Text>
+                <Card.Text className="duration5">
+                    <FontAwesomeIcon classname="clock1" icon={faClock}/>&nbsp;
+                    1hr 24mins
+                </Card.Text>
+
+                <Card.Text className="views1">
+                    <FontAwesomeIcon className="eye1" icon={faEye}/>&nbsp;
+                    21.3K views
+                </Card.Text>
                 <Card.Title className="movie-title5">Don't Breathe</Card.Title>
             </Card>
         </container>
