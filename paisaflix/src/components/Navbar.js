7a54af6fdf7c985adea5e11b65a006b74b1eef52
Antonella Ravaioli <5211016-antorava1@users.noreply.gitlab.com>
2021-09-20 01:26:55 -0300

    arreglos

--- paisaflix/src/components/Navbar.js
+++ paisaflix/src/components/Navbar.js
@@ -1,24 +1,28 @@
 import '../styles/navbar.css';
 import React from "react";
-import { Navbar, Nav, Container, Form} from 'react-bootstrap';
+import Hero from './Hero';
+import { Navbar, Nav, Container, Form, Image} from 'react-bootstrap';
 
 function NavBar () {
 
     return (
-        <Navbar expand="sm" variant="dark">
-            <Container>
-                <Navbar.Brand className="navbar-title">Paisaflix</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link className="nav-link active">Home</Nav.Link>
-                    <Nav.Link className="nav-link">Contact Us</Nav.Link>
-                    <Nav.Link className="nav-link">FAQ</Nav.Link>
-                    <Nav.Link className="nav-link">Pricing</Nav.Link>
-                    <Form className="d-flex">
-                        <Navbar.Text className="navbar-text">Soy Paisanx</Navbar.Text>
-                    </Form>
-                </Nav>
-            </Container>
-        </Navbar>
+        <>
+            <Navbar expand="sm" variant="dark">
+                <Container>
+                    <Navbar.Brand className="navbar-title">Paisaflix</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link className="nav-link home">Home</Nav.Link>
+                        <Nav.Link className="nav-link">Contact Us</Nav.Link>
+                        <Nav.Link className="nav-link">FAQ</Nav.Link>
+                        <Nav.Link className="nav-link">Pricing</Nav.Link>
+                        <Form className="d-flex">
+                            {/* <Image classNamw="search" src="..src/assets/navbar/search.svg"/> */}
+                            <Navbar.Text className="navbar-text">Soy Paisanx</Navbar.Text>
+                        </Form>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
     )
 }
 
