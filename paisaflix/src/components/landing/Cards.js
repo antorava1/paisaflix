3d9f11981f92818d73a3bd407d821cb0c30c54a1
Antonella Ravaioli <5211016-antorava1@users.noreply.gitlab.com>
2021-09-21 03:28:31 -0300

    landing card

--- paisaflix/src/components/landing/Cards.js
+++ paisaflix/src/components/landing/Cards.js
@@ -1,11 +1,11 @@
-import '../../styles/home/cards.css'
+import '../../styles/landing/cards.css'
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
 function Cards () {
     return (
         <container>
-            <h4 className="cards-title">Trailers</h4>
+            <h4 className="cards-title">Trailer</h4>
             <Card className="card">
                 <Card.Img src="https://frikivilla.com/wp-content/uploads/2019/01/avengers-endgame-marvel-frikivilla.jpg"/>
                 <Card.Body>
