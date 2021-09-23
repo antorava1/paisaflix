4110643c69c51e8e3a455886320064664f1893b2
Antonella Ravaioli <5211016-antorava1@users.noreply.gitlab.com>
2021-09-23 01:28:32 -0300

    featured

--- src/components/home/Featured.js
+++ src/components/home/Featured.js
@@ -23,25 +23,30 @@ export default class Featured extends Component {
             <div>
                 <h4 className="featured-title">Featured</h4>
                 <h4 className="view-more">View More</h4>
-                {this.state.movies.map((data, i) => {    
-                    return (
-                        <div className="featured-cards">
-                            <Card className="first-movie" key={i}>
-                                <Badge className="badge1">{data.genre}</Badge>
-                                <Card.Text className="duration1">
-                                    <FontAwesomeIcon className="clock1" icon={faClock}/>&nbsp;
-                                    {data.duration} mins
-                                </Card.Text>
-                                
-                                <Card.Text className="views1">
-                                    <FontAwesomeIcon className="eye1" icon={faEye}/>&nbsp;
-                                    {data.views} views
-                                </Card.Text>
-                                <Card.Title className="movie-title1">{data.name}</Card.Title>
-                            </Card>
-                    </div>
-                    )
-                })}
+                <div className="featured-cards">
+                    {this.state.movies.map((data, i) => {    
+                        return (
+                            <div className={"movie-item-"+ i}>
+                                <div className="movie-item">
+                                    <Card className="trailer-card" key={i}>
+                                    <img src={data.coverImage} className="featured-img" alt={data.name}></img>
+                                        <Badge className={"badge-item-"+ i}>{data.genre}</Badge>
+                                        <Card.Text className={"duration-item-"+ i}>
+                                            <FontAwesomeIcon className="clock1" icon={faClock}/>&nbsp;
+                                            {data.duration} mins
+                                        </Card.Text>
+                                        
+                                        <Card.Text className={"views-item-"+ i}>
+                                            <FontAwesomeIcon className="eye1" icon={faEye}/>&nbsp;
+                                            {data.views} views
+                                        </Card.Text>
+                                        <Card.Title className={"movie-title-item-"+ i}>{data.name}</Card.Title>
+                                    </Card>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         )
     }
