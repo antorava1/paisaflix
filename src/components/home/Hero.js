453857cdaef99e42ac72cbba420d0ffe093f919f
Antonella Ravaioli <5211016-antorava1@users.noreply.gitlab.com>
2021-09-24 04:42:58 -0300

    styles

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -47,19 +47,19 @@ export default class Hero extends Component {
         return (
             <>
                 <div>
-                    <div>
-                        <div className="hero-image">
-                            <img className="hero-bg" src={data.coverImage}  alt={data.name} ></img> 
-                        </div>
+                    <div className="hero-body">
                         <h1 className="movie-title">{data.name}</h1>
                         <Stars/>
                         <h6 className="movie-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</h6>
                         <h6 className="movie-genre">Genre: {data.genre}</h6>
                         <h6 className="movie-duration">Duration: {time}</h6>
                         <h6 className="movie-rating">Rating: {data.rating}</h6>
+                        <Link to="/landing"><Button className="btn">Watch Now</Button></Link>
+                        <Cards/>
                     </div>
-                    <Link to="/landing"><Button className="btn">Watch Now</Button></Link>
-                    <Cards/>
+                    <div className="hero-image">
+                            <img className="hero-bg" src={data.coverImage}  alt={data.name} ></img> 
+                        </div>
                 </div>
             </>
         )
